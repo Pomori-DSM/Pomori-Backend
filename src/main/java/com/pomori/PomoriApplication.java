f0e757e2bf77a85d7d00a8e5a6add01462664497
Daybreak312 <ty82afg12@gmail.com>
2024-09-12 12:10:16 +0900

    FEAT :: PomoriApplication

---
+++ src/main/java/com/pomori/PomoriApplication.java
@@ -0,0 +1,12 @@
+package com.pomori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomoriApplication.class, args);
+    }
+}
