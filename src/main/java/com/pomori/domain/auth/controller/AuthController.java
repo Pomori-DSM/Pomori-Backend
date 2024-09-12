fd0d53403f45ab02c79c31c298131e52a7979e99
Daybreak312 <ty82afg12@gmail.com>
2024-09-12 12:30:22 +0900

    FEAT :: Validation

--- src/main/java/com/pomori/domain/auth/controller/AuthController.java
+++ src/main/java/com/pomori/domain/auth/controller/AuthController.java
@@ -3,12 +3,17 @@ package com.pomori.domain.auth.controller;
 import com.pomori.domain.auth.dto.request.SignupRequest;
 import com.pomori.domain.auth.service.SignupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/auth")
 @RestController
 public class AuthController {
@@ -17,6 +22,8 @@ public class AuthController {
 
     @PostMapping("/signup")
     void signup(
+            @Valid
+            @NotNull
             @RequestBody
             SignupRequest signupRequest
     ) {
--- src/main/java/com/pomori/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/pomori/domain/auth/dto/request/SignupRequest.java
@@ -1,7 +1,18 @@
 package com.pomori.domain.auth.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public record SignupRequest(
+
+        @NotBlank
+        @Size(min = 3, max = 20)
         String username,
+
+        @NotBlank
+        @Size(min = 8, max = 20)
+        @Pattern(regexp = "^[A-Za-z\\d!@#$%&*]{8,20}$")
         String password
 ) {
 }
