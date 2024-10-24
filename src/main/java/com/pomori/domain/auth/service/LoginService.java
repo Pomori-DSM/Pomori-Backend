ce540c3064bd310a74a4a3322b4ddfbe2c8cffc5
Daybreak312 <ty82afg12@gmail.com>
2024-10-24 11:48:50 +0900

    REFACT :: 리팩토링

--- src/main/java/com/pomori/domain/auth/service/LoginService.java
+++ src/main/java/com/pomori/domain/auth/service/LoginService.java
@@ -4,14 +4,11 @@ import com.pomori.domain.annotation.ReadOnlyUseCase;
 import com.pomori.domain.auth.dto.request.AuthRequest;
 import com.pomori.domain.auth.dto.response.TokenResponse;
 import com.pomori.domain.exception.PomoriException;
-import com.pomori.domain.user.UserEntity;
 import com.pomori.domain.user.repository.UserRepository;
 import com.pomori.infra.security.Tokenizer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @ReadOnlyUseCase
@@ -25,18 +22,15 @@ public class LoginService {
 
     public TokenResponse login(final AuthRequest request) {
 
-        final Optional<UserEntity> user = userRepository.findByUsername(request.username());
+        final var user = userRepository.findByUsername(request.username())
+                .orElseThrow(() -> new PomoriException(HttpStatus.NOT_FOUND, "User not found"));
 
-        if (user.isEmpty()) {
-            throw new PomoriException(HttpStatus.NOT_FOUND, "User not found");
-        }
-
-        if (!passwordEncoder.matches(request.password(), user.get().getPassword())) {
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
             throw new PomoriException(HttpStatus.UNAUTHORIZED, "Incorrect password");
         }
 
         return new TokenResponse(
-                tokenizer.tokenize(user.get().getUsername())
+                tokenizer.tokenize(user.getUsername())
         );
     }
 }
